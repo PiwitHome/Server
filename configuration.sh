45c00f8f840f361097d14b3eb44a41258aa7dc38
PiwitHome <44818897+PiwitHome@users.noreply.github.com>
2018-11-16 17:36:50 +0100

    Update configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -2,7 +2,7 @@
 
 echo "####install FHEM####"
 if ! grep -q "^deb http://debian.fhem.de/nightly/ /" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-	wget -qO - http://debian.fhem.de/archive.key | apt-key add -
+	sudo wget -qO - http://debian.fhem.de/archive.key | sudo apt-key add -
 	sudo sh -c 'echo "deb http://debian.fhem.de/nightly/ /" >> /etc/apt/sources.list'
 	sudo apt-get update
 	sudo apt-get -y install fhem
@@ -17,39 +17,83 @@ if ! sudo grep -q "^fhem ALL=(ALL) NOPASSWD:/usr/bin/nmap" /etc/sudoers; then
 	sudo sh -c "echo \"fhem ALL=(ALL) NOPASSWD:/usr/bin/nmap\" >> /etc/sudoers"
 fi
 
-echo "####edit config####"
-#https://forum.fhem.de/index.php/topic,66616.0.html
-echo -en "attr WEB editConfig 1\nquit\n" | nc localhost 7072
+echo "####FHEM Config####"
+#activate telnet 
+if !(sudo grep -Po "serverVersion \K\d+\.*\d*" /opt/fhem/fhem.cfg | grep -q  "0.02"); then
+        echo "stop fhem for new configuration"
+        sudo service fhem stop
+		sudo sed -i '$ a define telnetPort telnet 7072 global' /opt/fhem/fhem.cfg
+		sudo service fhem start
+fi
 
-echo "####broadlink dependency####"
-sudo apt-get -y install libcrypt-cbc-perl
-sudo apt-get -y install libcrypt-rijndael-perl
-sudo apt -y install libssl-dev
-sudo cpan Crypt/OpenSSL/AES.pm
+# if no server version available, then initial configure and set to version 0.01
+if !sudo grep -Po "serverVersion \K\d+\.*\d*" /opt/fhem/fhem.cfg; then
+	echo "####broadlink dependency####"
+	sudo apt-get -y install libcrypt-cbc-perl
+	sudo apt-get -y install libcrypt-rijndael-perl
+	sudo apt -y install libssl-dev
+	sudo cpan Crypt/OpenSSL/AES.pm
+	
+	echo "####install snips####"
+	sudo apt-get -y install apt-transport-https
+	sudo apt-get update
+	sudo apt-get install -y dirmngr
+
+	sudo bash -c  'echo "deb   https://raspbian.snips.ai/stretch stable main" > /etc/apt/sources.list.d/snips.list'
+	sudo apt-key adv --keyserver pgp.surfnet.nl --recv-keys D4F50CDCA10A2849
+	sudo apt-get update
+	sudo apt-get install -y snips-platform-voice
+	sudo apt-get install -y snips-watch
+
+	echo "####configure snips####"
+	sudo rm -r /usr/share/snips/assistant
+	sudo unzip /home/pi/Server/DE-Snips/assistant_proj.zip -d /usr/share/snips/
+	sudo chmod 777 /usr/share/snips/assistant
+	sudo systemctl restart "snips*"
+
+	echo "####MQTT for FHEM####"
+	sudo cpan install Net::MQTT::Simple::SSL
+	sudo cpan install Net::MQTT::Constants
+	sudo apt-get -y install mosquitto mosquitto-clients
+	
+	echo "####install nmap####"
+	apt-get install -y libnmap-parser-perl
+	
+	echo "####configure nmap####"
+	echo -en '\
+	define ipScanner Nmap 192.168.2.100/24;\
+	attr ipScanner interval 300;\
+	attr ipScanner sudo 1;\
+	attr ipScanner userattr piwit;\
+	\nquit\n' | nc localhost 7072
+	echo -en '\
+	define nIPScannerSetIP notify ipScanner:running {my $a=`ip -f inet addr show | grep -Po "inet \\\K[\\\d.]+/24"`;;$a=~s/\\n//g;;Log 1, $a;; fhem("modify ipScanner ".$a);;}\
+	\nquit\n' | nc localhost 7072
+	
+	echo "####configure broadlink####"
+	
+		
+	
+	# set first serverVersion
+	echo -en "attr global userattr serverVersion\nquit\n" | nc localhost 7072
+	echo -en "attr global serverVersion 0.01\nquit\n" | nc localhost 7072 #https://forum.fhem.de/index.php/topic,66616.0.html
+	echo -en "save\nquit\n" | nc localhost 7072
+fi
 
 
-echo "####install snips####"
-sudo apt-get -y install apt-transport-https
-sudo apt-get update
-sudo apt-get install -y dirmngr
 
-sudo bash -c  'echo "deb   https://raspbian.snips.ai/$(lsb_release -cs) stable main" > /etc/apt/sources.list.d/snips.list'
+#version 0.02
+if sudo grep -Po "serverVersion \K\d+\.*\d*" /opt/fhem/fhem.cfg | grep -q  "0.01"; then
+ 
+# sudo sed 's/.*attr global serverVersion.*/attr global serverVersion 0.02/' /opt/fhem/fhem.cfg
+else
+ echo  no update
+fi
 
-sudo apt-key adv --keyserver pgp.mit.edu --recv-keys D4F50CDCA10A2849
-sudo apt-get update
-sudo apt-get install -y snips-platform-voice
-sudo apt-get install -y snips-watch
 
-echo "####configure snips####"
-sudo rm -r /usr/share/snips/assistant
-sudo unzip DE-Snips/assistant_proj.zip -d /usr/share/snips/
-sudo chmod 777 /usr/share/snips/assistant
-sudo systemctl restart "snips*"
 
-echo "####MQTT for FHEM####"
-sudo cpan install Net::MQTT::Simple::SSL
-sudo cpan install Net::MQTT::Constants
-sudo apt-get -y install mosquitto mosquitto-clients
+
+
 
 
 
