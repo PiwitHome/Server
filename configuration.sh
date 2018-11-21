2575c0adbf76ea18edc9a67eae8885212ebfc067
PiwitHome <44818897+PiwitHome@users.noreply.github.com>
2018-11-21 21:14:12 +0100

    Update configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#https://github.com/PiwitHome/Server.git 
+
 echo "####install FHEM####"
 if ! grep -q "^deb http://debian.fhem.de/nightly/ /" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
 	sudo wget -qO - http://debian.fhem.de/archive.key | sudo apt-key add -
@@ -28,12 +30,6 @@ fi
 
 # if no server version available, then initial configure and set to version 0.01
 if !sudo grep -Po "serverVersion \K\d+\.*\d*" /opt/fhem/fhem.cfg; then
-	echo "####broadlink dependency####"
-	sudo apt-get -y install libcrypt-cbc-perl
-	sudo apt-get -y install libcrypt-rijndael-perl
-	sudo apt -y install libssl-dev
-	sudo cpan Crypt/OpenSSL/AES.pm
-	
 	echo "####install snips####"
 	sudo apt-get -y install apt-transport-https
 	sudo apt-get update
@@ -70,10 +66,32 @@ if !sudo grep -Po "serverVersion \K\d+\.*\d*" /opt/fhem/fhem.cfg; then
 	define nIPScannerSetIP notify ipScanner:running {my $a=`ip -f inet addr show | grep -Po "inet \\\K[\\\d.]+/24"`;;$a=~s/\\n//g;;Log 1, $a;; fhem("modify ipScanner ".$a);;}\
 	\nquit\n' | nc localhost 7072
 	
+	echo "####install broadlink####"
+	sudo apt-get -y install libcrypt-cbc-perl
+	sudo apt-get -y install libcrypt-rijndael-perl
+	sudo apt -y install libssl-dev
+	sudo cpan Crypt/OpenSSL/AES.pm
 	echo "####configure broadlink####"
+	echo -en '\
+	define nBroadlinkUpdateIP notify ipScanner:.*_macVendor:.*Broadlin.* {my $macvendortomac=$EVTPART0;;$macvendortomac=~s/_macVendor:/_macAddress/g;;my $ip=$macvendortomac;;$ip=~s/_macAddress//g;;my $mac=ReadingsVal("ipScanner",$macvendortomac,"noIP");;my $broadlinkname="Broadlink_".$mac;;$broadlinkname=~s/:/_/g;;if(exists($defs{$broadlinkname})&&InternalVal($broadlinkname,"HOST","noIP") ne $ip){fhem("modify ".$broadlinkname." ".$ip." ".$mac." rmpro");;fhem("set ".$broadlinkname." getTemperature");;fhem("save");;}elsif(!exists($defs{$broadlinkname})){fhem("define ".$broadlinkname." Broadlink ".$ip." ".$mac." rmpro");;fhem("set ".$broadlinkname." getTemperature");;fhem("save");;};;};\
+	attr nBroadlinkUpdateIP userattr piwit;\
+	\nquit\n' | nc localhost 7072
+	
+	echo "####install YeeLight####"
+	sudo cpan install JSON::XS
+	echo -en '\
+	update all https://raw.githubusercontent.com/thaliondrambor/32_YeeLight.pm/master/controls_YeeLight.txt;\
+	reload 32_YeeLight.pm;\
+	\nquit\n' | nc localhost 7072
+	
+	echo "####configure YeeLight####"
+	#define YeeLight_34_CE_00_8B_63_93 YeeLight 192.168.2.105
+	echo -en '\
+	define nYeelightUpdateIP notify ipScanner:.*_ip:.* {my $iptomac=$EVTPART0;;$iptomac=~s/_ip:/_macAddress/g;;my $mac=ReadingsVal("ipScanner",$iptomac,"noIP");;if(index($mac,"34:CE:00")>=0){my $yeename="YeeLight_".$mac;;$yeename=~s/:/_/g;;if(exists($defs{$yeename})&&InternalVal($yeename,"HOST","noIP") ne $EVTPART1){fhem("modify ".$yeename." ".$EVTPART1);;}elsif(!exists($defs{$yeename})){fhem("define ".$yeename." YeeLight ".$EVTPART1);;};;};;};\
+	attr nYeelightUpdateIP userattr piwit;\
+	\nquit\n' | nc localhost 7072
 	
 		
-	
 	# set first serverVersion
 	echo -en "attr global userattr serverVersion\nquit\n" | nc localhost 7072
 	echo -en "attr global serverVersion 0.01\nquit\n" | nc localhost 7072 #https://forum.fhem.de/index.php/topic,66616.0.html
